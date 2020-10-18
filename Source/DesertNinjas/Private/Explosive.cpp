7678dfb52be446ac3acbab60b5aa063bde764fd8
David Barel - Game Server <david@envisability.com>
2020-10-18 16:58:33 -0700

    Game mechanics::Explosive mushroom; adding various styling packs

---
+++ Source/DesertNinjas/Private/Explosive.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../Source/DesertNinjas/Public/Explosive.h"
+
+
+#include "../Source/DesertNinjas/DesertNinjasCharacter.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Sound/SoundCue.h"
+// #include "Enemy.h"
+#include "Kismet/GameplayStatics.h"
+
+AExplosive::AExplosive()
+{
+	Damage = 15.f;
+}
+
+void AExplosive::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	UE_LOG(LogTemp, Warning, TEXT("Begin overlap on explosive"));
+
+	if (OtherActor)
+	{
+		ADesertNinjasCharacter* Main = Cast<ADesertNinjasCharacter>(OtherActor);
+		/*AEnemy* Enemy = Cast<AEnemy>(OtherActor);*/
+		if (Main)
+		{
+			OnExplosionBP(Main);
+			if (OverlapParticles)
+			{
+				UGameplayStatics::SpawnEmitterAtLocation(GetWorld(),
+					OverlapParticles, GetActorLocation(), FRotator(0.f), true);
+			}
+			if (OverlapSound)
+			{
+				UGameplayStatics::PlaySound2D(this, OverlapSound);
+			}
+			UGameplayStatics::ApplyDamage(OtherActor, Damage, nullptr, this, DamageTypeClass);
+
+			Destroy();
+		}
+	}
+}
+
+void AExplosive::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+
+	UE_LOG(LogTemp, Warning, TEXT("Begin overlap on explosive"));
+
+	//UE_LOG(LogTemp, Warning, TEXT("Explosive::OnOverlapEnd()"));
+}
---
+++ Source/DesertNinjas/Public/Pickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESERTNINJAS_API APickup : public AItem
+{
+	GENERATED_BODY()
+	
+};
