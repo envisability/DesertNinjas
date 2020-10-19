ce35560b7f73e0617be27994fc84e29c6e45a666
David Barel - Game Server <david@envisability.com>
2020-10-18 19:04:20 -0700

    Adding assets and completing pickups

--- Source/DesertNinjas/Public/Pickup.h
+++ Source/DesertNinjas/Public/Pickup.h
@@ -14,4 +14,15 @@ class DESERTNINJAS_API APickup : public AItem
 {
 	GENERATED_BODY()
 	
+public:
+
+	APickup();
+
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pickup")
+		void OnPickupBP(class ADesertNinjasCharacter* Target);
+
 };
